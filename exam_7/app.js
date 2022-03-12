7d64f7bc692302b79228118fe2e840456715e356
emily-hong <emilyhong4659@gmail.com>
2022-03-12 16:27:28 +0900

    feat:add exam7

---
+++ exam_7/app.js
@@ -0,0 +1,5 @@
+function sendGreeting(message, userName) {
+    console.log("".concat(message, ", ").concat(userName));
+}
+// void: 아무것도 반환하지 않는 함수의 반환 값으로만 사용될 수 있는 타입
+sendGreeting('Hello', 'Make');
---
+++ exam_7/app.ts
@@ -0,0 +1,6 @@
+function sendGreeting(message: string, userName: string): void {
+  console.log(`${message}, ${userName}`);
+}
+// void: 아무것도 반환하지 않는 함수의 반환 값으로만 사용될 수 있는 타입
+sendGreeting('Hello', 'Make');
+
